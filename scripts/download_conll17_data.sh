7d4f189a99c939245313dd5c1d5e6f14b4a5d403
jbar-ry <james.barry26@mail.dcu.ie>
2020-07-02 00:03:16 +0100

    update var names

--- scripts/download_conll17_data.sh
+++ scripts/download_conll17_data.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# download handle for UD2017 data for Irish
+# download handle for CoNLL 2017 data for Irish
 UD_CC="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1989/Irish-annotated-conll17.tar?sequence=23&isAllowed=y"
 ARCHIVE="ud_2017_data.tgz"
 
-DATASET_DIR="data/ga/UD2017-data"
+DATASET_DIR="data/ga/conll17"
 mkdir -p $DATASET_DIR
 
-echo "Downloading data from UD 2017..."$'\n'
+echo $'\n'"Downloading data from CoNLL 2017..."$'\n'
 curl ${UD_CC} -o ${ARCHIVE}
 tar -xf ${ARCHIVE} -C ${DATASET_DIR}
 mv ${ARCHIVE} $DATASET_DIR
@@ -17,32 +17,31 @@ UDPIPE_ga="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-
 UDPIPE_MODEL_DIR=udpipe_models
 mkdir -p ${UDPIPE_MODEL_DIR}
 
-echo "Downloading UDPipe model for ga..."$'\n'
+echo $'\n'"Downloading UDPipe model for ga..."$'\n'
 curl ${UDPIPE_ga} -o ga.udpipe
 mv ga.udpipe ${UDPIPE_MODEL_DIR}
 
 # convert files to raw text
 data=$(find "$DATASET_DIR/Irish" -type f -name "*.xz" )
-echo "found the following files:"$'\n'
+echo $'\n'"found the following files:"$'\n'
 echo "$data"
 
-
 # downloaded files are in x-zip format
-for xzf in ${data}; do
-	echo "processing: ${xzf}"
+for compressed in ${data}; do
+  echo "processing: ${compressed}"
 	
-	# decompress file
-	unxz ${xzf}
+  # decompress file
+  unxz ${compressed}
 	
-	base="$(basename -- ${xzf})"
+  base="$(basename -- ${compressed})"
+  # remove file-extension
+  filestring="$(echo $base | cut -f1 -d ".")"
+  conllu_file=${DATASET_DIR}/Irish/${filestring}.conllu
+  
+  # convert conllu to text
+  perl scripts/conllu_to_text.pl --language=ga_idt < ${conllu_file}  > ${DATASET_DIR}/${filestring}.txt
 	
-	# remove file-extension
-	filestring="$(echo $base | cut -f1 -d".")"
-	conllu_file=${DATASET_DIR}/Irish/${filestring}.conllu
-	# convert conllu to text
-	# TODO: this file needs to be downloaded and put in a shared location
-        perl ~/ud_folder/conllu_to_text.pl --language=ga_idt < ${conllu_file}  > ${DATASET_DIR}/${filestring}.txt
-	
-	# use udpipe model to tokenize/segment the raw text:
-	udpipe --tokenize --output horizontal ${UDPIPE_MODEL_DIR}/ga.udpipe ${DATASET_DIR}/${filestring}.txt > ${DATASET_DIR}/${filestring}_tokenised.txt
+  # use udpipe model to tokenize/segment the raw text:
+  #udpipe --tokenize --output horizontal ${UDPIPE_MODEL_DIR}/ga.udpipe ${DATASET_DIR}/${filestring}.txt > ${DATASET_DIR}/${filestring}_tokenised.txt
+
 done
