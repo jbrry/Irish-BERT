a201280e9e1405d52fb569930b540e7baedd555e
jbar-ry <james.barry26@mail.dcu.ie>
2020-09-18 12:55:02 +0100

    dont unpack text file

--- scripts/download_oscar_data.sh
+++ scripts/download_oscar_data.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# using de-duplicated source / only shuffled versions are available publicly
-IRISH_OSCAR_DEDUP="https://oscar-public.huma-num.fr/shuffled/ga_dedup.txt.gz"
 IRISH_OSCAR_UNSHUFFLED="" # you need to contact OSCAR maintainers for un-shuffled version
-OUTDIR=data/ga/oscar-unshuffled
+OUTDIR=data/ga/oscar
 
 mkdir -p $OUTDIR
 cd $OUTDIR
-wget $IRISH_OSCAR_DEDUP
-gunzip ga_dedup.txt.gz
+wget $IRISH_OSCAR_UNSHUFFLED
 
 echo "done"
