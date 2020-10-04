33c4e45399067d99adf2525f43b5b4f498506d61
Jbar-ry <james.barry26@mail.dcu.ie>
2020-10-04 13:01:34 +0100

    Update filtering files

---
+++ scripts/filter_corpora.py
@@ -0,0 +1,41 @@
+"""
+Filter specified datasets/corpora.
+
+Usage: python3 scripts/filter_corpora.py --datasets <dataset(s)>
+
+Requires running scripts/text_processor.py first.
+
+"""
+import sys
+import subprocess
+from argparse import ArgumentParser
+
+def argparser():
+    parser = ArgumentParser(
+        description='Filter corpora using OpusFilter.'
+    )
+    parser.add_argument(
+        '--datasets',
+        type=str,
+        help='Specify the datasets to download.',
+        choices={
+            'conll17',
+            'gdrive',
+            'oscar',
+        },
+        nargs='+',
+    )
+    return parser
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    config_dir="configs/opusfilter/"
+
+    for dataset in args.datasets:
+        print(f"Filtering {dataset} data.")
+        cfg = config_dir + "filter_{}.yaml".format(dataset)
+        cmd = f"opusfilter {cfg}"
+        rcmd = subprocess.call(cmd, shell=True)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
