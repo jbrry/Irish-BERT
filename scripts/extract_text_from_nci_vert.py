5ac5d8b92b0f0032e5f242baffb1c8b0dc1731a4
Joachim Wagner <jowagner@users.noreply.github.com>
2020-10-15 17:39:34 +0100

    ampersand-related replacements

--- scripts/extract_text_from_nci_vert.py
+++ scripts/extract_text_from_nci_vert.py
@@ -20,13 +20,52 @@ empty_line_after_document = True
 print_title = True
 print_author = True
 
+num_amp_tokens = '38 60 147 148 205 218 225 233 237 243 250'.split()
+
+def print_sentence(list_of_tokens):
+    text = ' '.join(list_of_tokens)
+    if not text or text.isspace():
+        sys.sdterr.write('Empty sentence with non-empty list of tokens.\n')
+        return
+    amp_replacement = False
+    first = True
+    while first or modified:
+        backup = text
+        if '&quot;' in text:
+            text = text.replace('&quot;',  '"')
+            #text = '@@amp.quot: ' + text
+        for token, char in [
+            ('lt',  '<'),
+            ('gt',  '>'),
+            ('amp', '&'),
+            ('quot', '"'),
+            # apos not observed
+        ]:
+            source = '& %s ;' %token
+            if source in text:
+                text = text.replace(source,  char)
+                #text = '@@amp_%s: %s' %(token, text)
+        for token in num_amp_tokens:
+            source = '& #%s ;' %token
+            if source in text:
+                text = text.replace(source, unichr(int(token)).encode('utf-8'))
+                #text = '@@amp#%s: %s' %(token, text)
+        modified = backup != text
+        if modified:
+            amp_replacement = True
+        # add code here for other types of replacements not involing ampersands
+        first = False
+    #if amp_replacement:
+    #    text = '@@amp: ' + text
+    print text
+
 sentence = []
 while True:
     line_no += 1
     line = sys.stdin.readline()
     if not line:
         if sentence:
-            print ' '.join(sentence)
+            print_sentence(sentence)
         #print '@@EOF'
         break
     try:
@@ -38,7 +77,7 @@ while True:
     byte_offset += len(line)
     if line.startswith('<'):
         if sentence:
-            print ' '.join(sentence)
+            print_sentence(sentence)
             sentence = []
             if empty_line_after_sentence:
                 print
@@ -48,7 +87,7 @@ while True:
             line = line.rstrip()
             #print line
             if ' & ' in line:
-                sys.stderr.write('` & ` in xml tag. Escaping it as ` &amp; `: %s\n' %line)
+                sys.stderr.write('Found ` & ` in xml tag. Escaping it as ` &amp; `: %s\n' %line)
                 line = line.replace(' & ', ' &amp; ')
             doc_attributes = xml.etree.ElementTree.fromstring(
                 '<?xml version="1.0" encoding="utf-8"?>%s</doc>' %line
@@ -64,6 +103,9 @@ while True:
                 print doc_attributes['author'].encode('utf-8')
                 if empty_line_after_sentence:
                     print
+        if line.startswith('<g'):
+            # glue tag or some other unsupported tag
+            raise NotImplementedError
     else:
         sentence.append(line.split()[0])
 
