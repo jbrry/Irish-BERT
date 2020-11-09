3e3055d7d18ba044c257b881d18db9c67c4f40f3
jbar-ry <james.barry26@mail.dcu.ie>
2020-11-09 14:18:43 +0000

    segment oscar data to line by line format

--- scripts/download_scripts/download_oscar_data.sh
+++ scripts/download_scripts/download_oscar_data.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
-IRISH_OSCAR_UNSHUFFLED="" # you need to contact OSCAR maintainers for un-shuffled version
+IRISH_OSCAR_UNSHUFFLED="https://oscar-public.huma-num.fr/f1f454290e16df612b59e17154d8725a61f42d02/unshuffled/ga.txt.gz" # you need to contact OSCAR maintainers for un-shuffled version
 OUTDIR=data/ga/oscar/raw
 
-mkdir -p $OUTDIR
-cd $OUTDIR
+# download handle for UDPipe v2.5 Irish model
+UDPIPE_ga="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3131/irish-idt-ud-2.5-191206.udpipe?sequence=39&isAllowed=y"
+UDPIPE_MODEL_DIR=udpipe_models
 
-wget $IRISH_OSCAR_UNSHUFFLED
+# Download UDPipe
+if [ -s "$UDPIPE_MODEL_DIR" ]; then
+    echo "$UDPIPE_MODEL_DIR exists, skipping download." >&2
+else
+    mkdir -p ${UDPIPE_MODEL_DIR}
+    echo $'\n'"Downloading UDPipe model for ga..."$'\n'
+    curl ${UDPIPE_ga} -o ga.udpipe
+    mv ga.udpipe ${UDPIPE_MODEL_DIR}
+fi  
 
-echo "done"
+# Download Oscar corpus
+if [ -s "$OUTDIR" ]; then
+    echo "$OUTDIR exists, skipping download." >&2
+else
+    mkdir -p ${OUTDIR}
+    echo $'\n'"Downloading Oscar data for ga..."$'\n'
+    cd $OUTDIR
+    wget $IRISH_OSCAR_UNSHUFFLED
+fi  
+
+if [[ -f "ga.txt.gz" ]]; then
+    echo "ga.txt.gz exists. Decompressing."
+    gunzip ga.txt.gz
+fi
+	
+# use udpipe model to tokenize/segment the raw text (otherwise the lines are not line-by-line):
+# this is important for corpus filtering, so we are not discarding more sentences than necessary.
+udpipe --tokenize --output horizontal ${UDPIPE_MODEL_DIR}/ga.udpipe ${OUTDIR}/ga.txt > ${OUTDIR}/ga_tokenised.txt
+
+# zip text file
+bzip2 ${OUTDIR}/ga_tokenised.txt
+
