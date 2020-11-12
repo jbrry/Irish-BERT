1847c267f6a7199b606efe56d04e726826c45d6a
jbar-ry <james.barry26@mail.dcu.ie>
2020-11-12 22:31:29 +0000

    increase vocab size and change output dir

--- scripts/convert_bert_original_tf_checkpoint_to_pytorch.sh
+++ scripts/convert_bert_original_tf_checkpoint_to_pytorch.sh
@@ -8,16 +8,23 @@ test -z $2 && echo "Missing BERT model type: ga_bert or multilingual_bert"
 test -z $2 && exit 1
 BERT_MODEL=$2
 
-MODEL_DIR=models/${BERT_MODEL}
+test -z $3 && echo "Missing model file description, 'NCI_filtered_none'"
+test -z $3 && exit 1
+FILE_DESC=$3
 
-OUT_DIR=${MODEL_DIR}/pretraining_output_${STEPS}
-PYTORCH_OUTDIR=${MODEL_DIR}/pytorch_models/${BERT_MODEL}_${STEPS}
+MODEL_DIR=models/${BERT_MODEL}
+OUT_DIR=${MODEL_DIR}/pretraining_output_${STEPS}-${FILE_DESC}
+PYTORCH_OUTDIR=${MODEL_DIR}/pytorch_models/pretraining_output_${STEPS}-${FILE_DESC}
 
 mkdir -p ${OUT_DIR}
 mkdir -p ${PYTORCH_OUTDIR}
 
-BERT_CONFIG=${MODEL_DIR}/bert_config.json  
-cp $BERT_CONFIG $PYTORCH_OUTDIR
+# make sure to `cat vocab.txt | wc -l` to get the size of the vocabulary for the config.
+BERT_CONFIG=${MODEL_DIR}/bert_config.json
+WIKI_BERT_DIR="../wiki-bert-pipeline"
+BERT_VOCAB=${WIKI_BERT_DIR}/data/${FILE_DESC}/ga/wordpiece/cased/vocab.txt
+cp ${BERT_CONFIG} ${PYTORCH_OUTDIR}
+cp ${BERT_VOCAB} ${PYTORCH_OUTDIR}
 
 python -m transformers.convert_bert_original_tf_checkpoint_to_pytorch \
   --tf_checkpoint_path ${OUT_DIR}/model.ckpt-${STEPS} \
