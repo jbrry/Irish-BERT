192f2dcddfbfa1e77348a18354422b075ea7f2da
jbar-ry <james.barry26@mail.dcu.ie>
2020-11-18 11:48:50 +0000

    change to using rclone and remove download of UDPipe

--- scripts/download_scripts/download_oscar_data.sh
+++ scripts/download_scripts/download_oscar_data.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-IRISH_OSCAR_UNSHUFFLED="" # you need to contact OSCAR maintainers for un-shuffled version
+# downloads the Oscar corpus from Google Drive using rclone: https://rclone.org/
+# use double-quotes if the path contains spaces
 OUTDIR=data/ga/oscar/raw
-
-# download handle for UDPipe v2.5 Irish model
-UDPIPE_ga="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3131/irish-idt-ud-2.5-191206.udpipe?sequence=39&isAllowed=y"
-UDPIPE_MODEL_DIR=udpipe_models
-
-# Download UDPipe
-if [ -s "$UDPIPE_MODEL_DIR" ]; then
-    echo "$UDPIPE_MODEL_DIR exists, skipping download." >&2
-else
-    mkdir -p ${UDPIPE_MODEL_DIR}
-    echo $'\n'"Downloading UDPipe model for ga..."$'\n'
-    curl ${UDPIPE_ga} -o ga.udpipe
-    mv ga.udpipe ${UDPIPE_MODEL_DIR}
-fi  
 
 # Download Oscar corpus
 if [ -s "$OUTDIR" ]; then
@@ -23,8 +10,8 @@ if [ -s "$OUTDIR" ]; then
 else
     mkdir -p ${OUTDIR}
     echo $'\n'"Downloading Oscar data for ga..."$'\n'
-    cd $OUTDIR
-    wget $IRISH_OSCAR_UNSHUFFLED
+    rclone copy "gdrive:Theme A DCU/ga_BERT/BERT_Preprocessing/raw-corpora/oscar-ga-unshuffled.tar.bz2" $OUTDIR --bwlimit 1000M --transfers 1
 fi  
 
+echo "Done"
 
