b552f6a7d02a9df23dc158c3828990e277a28a65
jbar-ry <james.barry26@mail.dcu.ie>
2020-11-26 19:45:03 +0000

    update paracrawl download script

--- scripts/download_scripts/download_paracrawl_data.sh
+++ scripts/download_scripts/download_paracrawl_data.sh
@@ -6,11 +6,11 @@ SRC=ga
 TRG=en
 
 # OPUS corpus name, e.g. 'paracrawl'
-CORPUS=$1
+CORPUS=paracrawl
 echo "Using corpus $CORPUS"
-mkdir -p data/ga/opus/$CORPUS
+mkdir -p data/ga/$CORPUS
 
-OUTDIR=data/ga/opus/$CORPUS
+OUTDIR=data/ga/$CORPUS
 
 CONFIG=configs/opusfilter/${CORPUS}_${SRC}-${TRG}.yaml
 echo "Using configuration file: $CONFIG"
