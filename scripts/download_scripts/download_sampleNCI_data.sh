a2ed109a6592ce661a3b7cab8df5bbc619c419b9
Joachim Wagner <jowagner@users.noreply.github.com>
2020-12-05 10:31:25 +0000

    dataset "sampleNCI" with 1000 random NCI lines

---
+++ scripts/download_scripts/download_sampleNCI_data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# downloads NCI file from Google Drive using rclone: https://rclone.org/
+# the NCI does not require filtering so it is better to keep this file separate.
+# also, make sure to put a 0 in front of this filename in gdrive_filelist.csv so that it is not downloaded twice.
+
+# use double-quotes if the path contains spaces
+echo "Downloading data from Google Drive ..."
+
+OUTDIR=data/ga/sampleNCI/raw
+mkdir -p $OUTDIR
+
+if [[ -n $(rclone lsf "gdrive:Theme A DCU" 2> /dev/null) ]]; then
+    THEME_A_DCU="gdrive:Theme A DCU"
+else
+    if [[ -n $(rclone lsf "gdrive:Theme A/Theme A DCU" 2> /dev/null) ]]; then
+        THEME_A_DCU="gdrive:Theme A/Theme A DCU"
+    else
+        echo "Theme A DCU folder not found"
+        exit 1
+    fi
+fi
+
+rclone cat "${THEME_A_DCU}/Irish_Data/ForasNaGaeilge/new-extraction/sample-1000-with-line-numbers.txt" --bwlimit 1000M --transfers 1 | \
+    cut -f2 | bzip2 > ${OUTDIR}/sample-1000.txt.bz2
+
+echo "Done"
