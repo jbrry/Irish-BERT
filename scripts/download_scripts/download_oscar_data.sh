2b360205ce0aeb5c6dda3c97c9e893c0b144a172
Joachim Wagner <jowagner@users.noreply.github.com>
2021-02-16 12:12:45 +0000

    locate Theme A folder; check for errors

--- scripts/download_scripts/download_oscar_data.sh
+++ scripts/download_scripts/download_oscar_data.sh
@@ -3,6 +3,18 @@
 # downloads the Oscar corpus from Google Drive using rclone: https://rclone.org/
 # use double-quotes if the path contains spaces
 OUTDIR=data/ga/oscar/raw
+FILENAME=oscar-ga-unshuffled.tar.bz2
+
+if [[ -n $(rclone lsf "gdrive:Theme A DCU" 2> /dev/null) ]]; then
+    THEME_A_DCU="gdrive:Theme A DCU"
+else
+    if [[ -n $(rclone lsf "gdrive:Theme A/Theme A DCU" 2> /dev/null) ]]; then
+        THEME_A_DCU="gdrive:Theme A/Theme A DCU"
+    else
+        echo "Theme A DCU folder not found"
+        exit 1
+    fi
+fi
 
 # Download Oscar corpus
 if [ -s "$OUTDIR" ]; then
@@ -10,8 +22,13 @@ if [ -s "$OUTDIR" ]; then
 else
     mkdir -p ${OUTDIR}
     echo $'\n'"Downloading Oscar data for ga..."$'\n'
-    rclone copy "gdrive:Theme A DCU/ga_BERT/BERT_Preprocessing/raw-corpora/oscar-ga-unshuffled.tar.bz2" $OUTDIR --bwlimit 1000M --transfers 1
+    rclone copy "${THEME_A_DCU}/ga_BERT/BERT_Preprocessing/raw-corpora/${FILENAME}" $OUTDIR --bwlimit 1000M --transfers 1
+    if [ -f "${OUTDIR}/${FILENAME}" ]; then
+        echo "Done"
+    else
+        echo "Download failed"
+        rmdir "${OUTDIR}"
+        exit 1
+    fi
 fi  
-
-echo "Done"
 
