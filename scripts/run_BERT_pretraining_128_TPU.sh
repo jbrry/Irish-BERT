d2830b79e929bfd0f2a2d70b4758ea8099dda5e9
jbrry <james.barry26@mail.dcu.ie>
2021-02-17 18:19:46 +0000

    Complete GS locations

--- scripts/run_BERT_pretraining_128_TPU.sh
+++ scripts/run_BERT_pretraining_128_TPU.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Runs BERT on a TPU node
+# ./scripts/run_BERT_pretraining_128_TPU.sh 5000 tpu-gabert gabert conll17_gdrive_NCI_oscar_paracrawl_filtering_None
+
 test -z $1 && echo "Missing number of pre-training steps, e.g. '900000'"
 test -z $1 && exit 1
 STEPS=$1
@@ -22,28 +25,13 @@ NUM_WARMUP_STEPS=` expr $STEPS / 100`
 echo "Training BERT for $STEPS steps"
 echo "using $NUM_WARMUP_STEPS warmup steps ..."
 
-BERT_DIR=path/to/bert # on TPU node.
-DATA_DIR="gs://$BUCKET_NAME/data/gabert/pretraining_data/$FILE_DESC/ga/tfrecords/seq-128/"
+BERT_DIR=${HOME}/gabert/bert
+DATA_DIR="gs://$BUCKET_NAME/data/gabert/pretraining_data/$FILE_DESC/ga/tfrecords/seq-128"
+OUTPUT_DIR="gs://$BUCKET_NAME/data/gabert/model_output/${FILE_DESC}"
 
-OUTPUT_DIR="gs://$BUCKET_NAME/data/blah/blah/pretraining_output_${FILE_DESC}_${STEPS}"
+BERT_CONFIG=${HOME}/gabert/Irish-BERT/models/ga_bert/bert_config.json
 
-
-#MODEL_DIR="models/${BERT_MODEL}"
-#BERT_CONFIG="${MODEL_DIR}/bert_config.json"
-#OUTPUT_DIR="${MODEL_DIR}/pretraining_output_${FILE_DESC}_${STEPS}"
-
-#  --output_dir=gs://$bert_directory \
-mkdir -p $OUTPUT_DIR
-
-# if using multilingual BERT, first initialise from original checkpoint
-if [ "${BERT_MODEL}" = "multilingual_bert" ]; then   
-  DATA_DIR=${WIKI_BERT_DIR}/data/ga/tfrecords-multilingual_bert/seq-128
- 
-  ln -s $PWD/${MODEL_DIR}/bert_model.ckpt.index $PWD/${OUTPUT_DIR}/bert_model.ckpt.index
-  extra_args="--init_checkpoint=${OUTPUT_DIR}/bert_model.ckpt"
-else
-  extra_args=""
-fi
+#mkdir -p $OUTPUT_DIR
 
 # if [ $# -gt 3 ]; then
 #   echo "More than 3 arguments passed, assuming this is the step number to initialize from"
@@ -64,10 +52,9 @@ python3 ${BERT_DIR}/run_pretraining.py \
 	--max_seq_length=128 \
 	--max_predictions_per_seq=20 \
 	--num_train_steps=${STEPS} \
-    --save_checkpoint_steps=100000 \
+    	--save_checkpoint_steps=100000 \
 	--num_warmup_steps=${NUM_WARMUP_STEPS} \
 	--learning_rate=1e-4 \
-    --use_tpu=True \
-    --tpu_name=$tpu_name
-
+    	--use_tpu=True \
+    	--tpu_name=$TPU_NAME
 
