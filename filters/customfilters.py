83712ea90d6d807e45d3a8c1c646333576e71fd8
jbar-ry <james.barry26@mail.dcu.ie>
2021-02-18 14:08:50 +0000

    make customfilters pass empty lines

--- filters/customfilters.py
+++ filters/customfilters.py
@@ -1,21 +1,21 @@
 import opusfilter
-from opusfilter import FilterABC
 import string
 
 """
 This module includes some additional filters to the OpusFilter library.
 For a list of existing filters, see: https://github.com/Helsinki-NLP/OpusFilter/blob/nlingual-rebase/opusfilter/filters.py
 
-This module needs to be added to Python path:
-    e.g.: export PYTHONPATH=/home/user/ga_BERT/Irish-BERT/filters/
+This module needs to be added to Python path, e.g.:
+    export PYTHONPATH=/home/jbarry/spinning-storage/jbarry/ga_BERT/Irish-BERT/filters/
 """
 
 
-class PunctuationFilter(FilterABC):
+class PunctuationFilter(opusfilter.FilterABC):
     """Calculates the percentage of punctuation among characters of a sentence"""
 
-    def __init__(self, threshold=0.2, **kwargs):
+    def __init__(self, threshold=0.6, pass_empty=False, **kwargs):
         self.threshold = threshold
+        self.pass_empty = pass_empty
         super().__init__(**kwargs)
 
     def score(self, pairs):
@@ -24,20 +24,28 @@ class PunctuationFilter(FilterABC):
         for pair in pairs:
             for sent in pair:
                 words = sent.split()
-                num_chars = len([c for w in words for c in w])
-                num_punct_chars = len([c for w in words for c in w if c in punct])
-                punct_ratio = num_punct_chars / num_chars
+                if len(words) >= 1:
+                    num_chars = len([c for w in words for c in w])
+                    num_punct_chars = len([c for w in words for c in w if c in punct])
+                    punct_ratio = num_punct_chars / num_chars
+                else:
+                    if self.pass_empty:
+                        punct_ratio = 0
+                    else:
+                        punct_ratio = 1
+                
                 yield punct_ratio
 
     def accept(self, score):
         return score < self.threshold
 
 
-class DigitsFilter(FilterABC):
+class DigitsFilter(opusfilter.FilterABC):
     """Calculates the percentage of digits among characters of a sentence"""
 
-    def __init__(self, threshold=0.2, **kwargs):
+    def __init__(self, threshold=0.6, pass_empty=False, **kwargs):
         self.threshold = threshold
+        self.pass_empty = pass_empty
         super().__init__(**kwargs)
 
     def score(self, pairs):
@@ -46,9 +54,16 @@ class DigitsFilter(FilterABC):
         for pair in pairs:
             for sent in pair:
                 words = sent.split()
-                num_chars = len([c for w in words for c in w])
-                num_digit_chars = len([c for w in words for c in w if c in digits])
-                digit_ratio = num_digit_chars / num_chars
+                if len(words) >= 1:
+                    num_chars = len([c for w in words for c in w])
+                    num_digit_chars = len([c for w in words for c in w if c in digits])
+                    digit_ratio = num_digit_chars / num_chars
+                else:
+                    if self.pass_empty:
+                        digit_ratio = 0
+                    else:
+                        digit_ratio = 1
+
                 yield digit_ratio
 
     def accept(self, score):
