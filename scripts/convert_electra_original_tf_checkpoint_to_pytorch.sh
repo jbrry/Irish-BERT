aeea5e0170325e5bddc28cdb9d7f71689e851d8a
jbar-ry <james.barry26@mail.dcu.ie>
2021-03-11 23:44:39 +0000

    update checkpoint conversion scripts

--- scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh
+++ scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh
@@ -8,22 +8,22 @@ test -z $2 && echo "Missing model file description, e.g. 'conll17_gdrive_NCI_osc
 test -z $2 && exit 1
 FILE_DESC=$2
 
-# TF checkpoints are available on Google Drive
-CHECKPOINT="/home/jbarry/ga_BERT/Irish-BERT/models/ga_bert/output/gabert-electra/$FILE_DESC/models/electra-base-irish-cased"
+test -z $3 && echo "Missing model type, use 'generator' for MLM or 'discriminator' for classification"
+test -z $3 && exit 1
+MODEL_TYPE=$3
 
-CONFIG="models/ga_bert/electra_base_config.json"
-OUTDIR="/home/jbarry/ga_BERT/Irish-BERT/models/ga_bert/output/pytorch/gabert-electra/$FILE_DESC"
+GABERT_DIR="/home/jbarry/spinning-storage/jbarry/ga_BERT"
+# TF checkpoints are available on Google Drive
+CHECKPOINT="$GABERT_DIR/Irish-BERT/models/ga_bert/output/electra/conll17_gdrive_NCI_oscar_paracrawl_filtering_basic+char-1.0+lang-0.8/models/electra-base-irish-cased"
+CONFIG="$GABERT_DIR/Irish-BERT/models/ga_bert/electra_base_${MODEL_TYPE}_config.json"
+OUTDIR="$GABERT_DIR/Irish-BERT/models/ga_bert/output/pytorch/electra/$FILE_DESC/electra-base-irish-cased-$MODEL_TYPE-$STEPS/"
 
 mkdir -p $OUTDIR
 cp "$CONFIG" "$OUTDIR/config.json"
 # vocab file should be available from the same location on Gogle drive as $CHECKPOINT
-cp "$CHECKPOINT/vocab.txt" "$OUTDIR"
+cp "$GABERT_DIR/Irish-BERT/models/ga_bert/output/electra/$FILE_DESC/vocab.txt" "$OUTDIR"
 
-# use 'generator' for MLM or 'discriminator' for classification
-# e.g. --discriminator_or_generator=generator to be used with ElectraForMaskedLM
-MODEL_TYPE="generator"
-
-TRANSFORMERS_DIR="$HOME/ga_BERT/transformers"
+TRANSFORMERS_DIR="$GABERT_DIR/transformers"
 
 # Use the conversion script
 python "$TRANSFORMERS_DIR"/src/transformers/models/electra/convert_electra_original_tf_checkpoint_to_pytorch.py \
@@ -31,3 +31,7 @@ python "$TRANSFORMERS_DIR"/src/transformers/models/electra/convert_electra_origi
     --config_file="$CONFIG" \
     --pytorch_dump_path="$OUTDIR"/pytorch_model.bin \
     --discriminator_or_generator="$MODEL_TYPE"
+
+# pack model as expected by AlennNLP
+cd "$OUTDIR"
+tar -czf weights.tar.gz config.json pytorch_model.bin
