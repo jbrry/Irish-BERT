0e10871106a609abf0f70bb82ec69d3c964807a9
jbar-ry <james.barry26@mail.dcu.ie>
2021-03-11 17:43:32 +0000

    account for training at different seq_lens

--- scripts/calculate_training_tokens.py
+++ scripts/calculate_training_tokens.py
@@ -1,65 +1,53 @@
 
-def epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size):
+def epoch_calculator(name, batch_size, seq_len, steps, corpus_size, steps_proportion):
 
-    tokens_per_batch = (batch_size * max_seq_len)
+    tokens_per_batch = (batch_size * seq_len)
     total = tokens_per_batch * steps
     epochs = total / corpus_size
 
-    print(f"{name}: Tokens seen: {total}, Approx. {epochs:.2f} epochs.")
-
+    print(f"{name} {steps_proportion}% @ {seq_len}: Tokens seen: {total/1000000}M, Approx. {epochs:.2f} epochs.")
 
 # Original BERT paper
 name = "BERT"
 corpus_size = 3300000000
-max_seq_len = 512
+seq_len = 128
 batch_size = 256
-steps = 1000000
-epochs = epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size)
+steps = 900000
+steps_proportion = 90
+epochs = epoch_calculator(name, batch_size, seq_len, steps, corpus_size, steps_proportion)
 
-# GBERT-base
-name = "GBERT-base"
-corpus_size = 2000000000
-max_seq_len = 512
-batch_size = 128
-steps = 4000000
-epochs = epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size)
-
-# GBERT-large
-name = "GBERT-large"
-corpus_size = 2000000000
-max_seq_len = 512
-batch_size = 2048
-steps = 1000000
-epochs = epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size)
-
-# GELECTRA-base
-name = "GELECTRA-base"
-corpus_size = 2000000000
-max_seq_len = 512
+# Original BERT paper
+name = "BERT"
+corpus_size = 3300000000
+seq_len = 512
 batch_size = 256
-steps = 766000
-epochs = epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size)
-
-# GELECTRA-large
-name = "GELECTRA-large"
-corpus_size = 2000000000
-max_seq_len = 512
-batch_size = 1024
-steps = 766000
-epochs = epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size)
+steps = 100000
+steps_proportion = 10
+epochs = epoch_calculator(name, batch_size, seq_len, steps, corpus_size, steps_proportion)
 
 # GaBERT-base
 name = "GaBERT-base"
 corpus_size = 159429404 # cat ga/filtered-texts/* | wc = 7937938 159429404 919788128
-max_seq_len = 512
+seq_len = 128
 batch_size = 64
-steps = 1000000
-epochs = epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size)
+steps = 900000
+steps_proportion = 90
+epochs = epoch_calculator(name, batch_size, seq_len, steps, corpus_size, steps_proportion)
 
-# GELECTRA-base
+# GaBERT-base
+name = "GaBERT-base"
+corpus_size = 159429404 # cat ga/filtered-texts/* | wc = 7937938 159429404 919788128
+seq_len = 512
+batch_size = 64
+steps = 100000
+steps_proportion = 10
+epochs = epoch_calculator(name, batch_size, seq_len, steps, corpus_size, steps_proportion)
+
+# GaELECTRA-base
 name = "GaELECTRA-base"
 corpus_size = 159429404
-max_seq_len = 512
+seq_len = 512
 batch_size = 256
-steps = 1000000
-epochs = epoch_calculator(name, batch_size, max_seq_len, steps, corpus_size)
+steps = 250000
+steps_proportion = 100
+epochs = epoch_calculator(name, batch_size, seq_len, steps, corpus_size, steps_proportion)
