b65842413c23a3ed5cd48b6f8eb9a1d73f1ca3d7
jbar-ry <james.barry26@mail.dcu.ie>
2021-04-29 17:06:59 +0100

    use argparser

--- scripts/train_HF_tokenizer.py
+++ scripts/train_HF_tokenizer.py
@@ -4,55 +4,70 @@ import json
 
 from tokenizers import BertWordPieceTokenizer
 
-# Usage:
-# python scripts/train_HF_tokenizer.py /path/to/ga_BERT/wiki-bert-pipeline/data/conll17_gdrive_NCI_oscar_paracrawl_filtering_basic+char-1.0+lang-0.8/ga/filtered-concatenated/ga_filtered_all.txt vocabs
 
-training_file = sys.argv[1]
-outdir = sys.argv[2]
-number_unused = int(sys.argv[3])
+def argparser():
+    from argparse import ArgumentParser
+    ap = ArgumentParser()
+    ap.add_argument('--outdir', type=str,
+                    help='Where to write output.')
+    ap.add_argument('--number_unused', type=int,
+                    help='Number of unused entries to write.')
+    ap.add_argument('file', nargs='+')
+    return ap
 
-if not os.path.exists(outdir):
-    print(f"Creating output directory: {outdir}")
-    os.makedirs(outdir)
 
-tokenizer = BertWordPieceTokenizer(
-    clean_text=True,
-    handle_chinese_chars=False,
-    strip_accents=False,
-    lowercase=False, 
-)
+def main(argv):
+    args = argparser().parse_args(argv[1:])
 
-trainer = tokenizer.train(
-    training_file,
-    vocab_size=30000,
-    min_frequency=2,
-    show_progress=True,
-    special_tokens=['[PAD]', '[UNK]', '[CLS]', '[SEP]', '[MASK]'],
-    limit_alphabet=1000,
-    wordpieces_prefix="##"
-)
+    if not os.path.exists(args.outdir):
+        print(f"Creating output directory: {args.outdir}")
+        os.makedirs(args.outdir)
 
-# This will save the vocab as a json dictionary,
-# The vocab entries are located in ["model"]["vocab"]
-tokenizer.save(f"{outdir}/vocab.json", True)
+    tokenizer = BertWordPieceTokenizer(
+        clean_text=True,
+        handle_chinese_chars=False,
+        strip_accents=False,
+        lowercase=False, 
+    )
 
-with open(f"{outdir}/vocab.json") as json_file:
-    data = json.load(json_file)
-    vocab = data["model"]["vocab"]
+    training_file = args.file
+
+    trainer = tokenizer.train(
+        training_file,
+        vocab_size=30000,
+        min_frequency=2,
+        show_progress=True,
+        special_tokens=['[PAD]', '[UNK]', '[CLS]', '[SEP]', '[MASK]'],
+        limit_alphabet=1000,
+        wordpieces_prefix="##"
+    )
+
+    # This will save the vocab as a json dictionary,
+    # The vocab entries are located in ["model"]["vocab"]
+    tokenizer.save(f"{args.outdir}/vocab.json", True)
+
+    with open(f"{args.outdir}/vocab.json") as json_file:
+        data = json.load(json_file)
+        vocab = data["model"]["vocab"]
+        
+        words = []
+        for w in vocab:
+            words.append(w)
+        json_file.close()
+
+    with open(f"{args.outdir}/vocab.txt", "w") as txt_file:
+        for i, w in enumerate(words, start=1):
+            txt_file.write(w + "\n")
+            # most implementations write the first special symbol, then the unused entries
+            if i == 1:
+                for j in range(args.number_unused):
+                    txt_file.write(f"[unused{j}]" + "\n")
+
+        txt_file.close()
+        print(f"Finished writing vocabulary to {args.outdir}/vocab.txt")
     
-    words = []
-    for w in vocab:
-        words.append(w)
-    json_file.close()
+    return 0
 
-with open(f"{outdir}/vocab.txt", "w") as txt_file:
-    for i, w in enumerate(words, start=1):
-        txt_file.write(w + "\n")
-        # most implementations write the first special symbol, then the unused entries
-        if i == 1:
-            for j in range(number_unused):
-                txt_file.write(f"[unused{j}]" + "\n")
-
-    txt_file.close()
-    print(f"Finished writing vocabulary to {outdir}/vocab.txt")
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
 
