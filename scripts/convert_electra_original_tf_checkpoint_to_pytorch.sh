9c921822861bc8e99eecaae0e23be502a8658593
jbar-ry <james.barry26@mail.dcu.ie>
2021-05-25 19:10:38 +0100

    update electra pipeline

--- scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh
+++ scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh
@@ -4,7 +4,7 @@ test -z $1 && echo "Missing number of pre-training steps, e.g. '1000000'"
 test -z $1 && exit 1
 STEPS=$1
 
-test -z $2 && echo "Missing model file description, e.g. 'conll17_gdrive_NCI_oscar_paracrawl_filtering_basic+char-1.0+lang-0.8'"
+test -z $2 && echo "Missing model file description, e.g. 'conll17_gdrive_NCI_oscar_paracrawl_wiki_filtering_basic+char-1.0+lang-0.8'"
 test -z $2 && exit 1
 FILE_DESC=$2
 
@@ -14,15 +14,17 @@ MODEL_TYPE=$3
 
 # You may need to change this path according to how you set up this directory.
 GABERT_DIR="/home/${USER}/spinning-storage/${USER}/ga_BERT"
+
+CHECKPOINT="$GABERT_DIR/Irish-BERT/models/electra/${FILE_DESC}/models/electra-base-${FILE_DESC}"
+CONFIG="$GABERT_DIR/Irish-BERT/models/electra/electra_base_${MODEL_TYPE}_config.json"
+OUTDIR="$GABERT_DIR/Irish-BERT/models/electra/output/pytorch/$FILE_DESC/electra-base-${FILE_DESC}-${MODEL_TYPE}-${STEPS}"
+
 # TF checkpoints are available on Google Drive
-CHECKPOINT="$GABERT_DIR/Irish-BERT/models/ga_bert/output/electra/$FILE_DESC/models/electra-base-irish-cased"
-CONFIG="$GABERT_DIR/Irish-BERT/models/ga_bert/electra_base_${MODEL_TYPE}_config.json"
-OUTDIR="$GABERT_DIR/Irish-BERT/models/ga_bert/output/pytorch/electra/$FILE_DESC/electra-base-irish-cased-$MODEL_TYPE-$STEPS/"
+#CHECKPOINT="$GABERT_DIR/Irish-BERT/models/ga_bert/output/electra/$FILE_DESC/models/electra-base-irish-cased"
 
 mkdir -p $OUTDIR
 cp "$CONFIG" "$OUTDIR/config.json"
-# vocab file should be available from the same location on Gogle drive as $CHECKPOINT
-cp "$GABERT_DIR/Irish-BERT/models/ga_bert/output/electra/$FILE_DESC/vocab.txt" "$OUTDIR"
+cp "$GABERT_DIR/Irish-BERT/models/electra/$FILE_DESC/vocab.txt" "$OUTDIR"
 
 TRANSFORMERS_DIR="$GABERT_DIR/transformers"
 
