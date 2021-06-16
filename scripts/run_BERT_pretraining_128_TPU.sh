04e984214a58773977da7b19d51fef68dd785355
jbrry <james.barry26@mail.dcu.ie>
2021-06-16 14:54:21 +0000

    support mbert on tpu

--- scripts/run_BERT_pretraining_128_TPU.sh
+++ scripts/run_BERT_pretraining_128_TPU.sh
@@ -7,17 +7,21 @@ test -z $1 && echo "Missing number of pre-training steps, e.g. '900000'"
 test -z $1 && exit 1
 STEPS=$1
 
-test -z $2 && echo "Missing TPU Name, e.g. 'tpu-gabert'"
+test -z $2 && echo "Missing BERT model type: ga_bert or multilingual_bert"
 test -z $2 && exit 1
-TPU_NAME=$2
+BERT_MODEL=$2
 
-test -z $3 && echo "Missing Bucket Name, e.g. 'gabert'"
+test -z $3 && echo "Missing TPU Name, e.g. 'tpu-gabert'"
 test -z $3 && exit 1
-BUCKET_NAME=$3
+TPU_NAME=$3
 
-test -z $4 && echo "Missing model file description, e.g. list of corpora and filtering type: 'conll17_gdrive_NCI_oscar_paracrawl_filtering_None'"
+test -z $4 && echo "Missing Bucket Name, e.g. 'gabert'"
 test -z $4 && exit 1
-FILE_DESC=$4
+BUCKET_NAME=$4
+
+test -z $5 && echo "Missing model file description, e.g. list of corpora and filtering type: 'conll17_gdrive_NCI_oscar_paracrawl_filtering_None'"
+test -z $5 && exit 1
+FILE_DESC=$5
 
 # original BERT paper uses 1% as the number of warmup steps
 NUM_WARMUP_STEPS=` expr $STEPS / 100`
@@ -29,7 +33,14 @@ BERT_DIR=${HOME}/gabert/bert
 DATA_DIR="gs://$BUCKET_NAME/data/gabert-v2/pretraining_data/$FILE_DESC/ga/tfrecords/seq-128"
 OUTPUT_DIR="gs://$BUCKET_NAME/data/gabert-v2/model_output/${FILE_DESC}"
 
-BERT_CONFIG=${HOME}/gabert/Irish-BERT/models/ga_bert/bert_config.json
+BERT_CONFIG=${HOME}/gabert/Irish-BERT/models/${BERT_MODEL}/bert_config.json
+
+# if using multilingual BERT, first initialise from original checkpoint
+if [ "${BERT_MODEL}" = "multilingual_bert" ]; then    
+  extra_args="--init_checkpoint=${OUTPUT_DIR}/bert_model.ckpt"
+else
+  extra_args=""
+fi
 
 if [ $# -gt 4 ]; then
    echo "More than 4 arguments passed, assuming this is the step number to initialize from"
