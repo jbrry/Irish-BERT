addee3dc694b59a6c4f56d20bbabaefec07aa474
jbar-ry <james.barry26@mail.dcu.ie>
2021-06-29 13:00:46 +0100

    update electra checkpoint storage

--- scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh
+++ scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
+# /scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh ${CHKPT} ${BERT_MODEL} ${FILE_DESC} ${MODEL_TYPE}
+
 test -z $1 && echo "Missing number of pre-training steps, e.g. '1000000'"
 test -z $1 && exit 1
 STEPS=$1
 
-test -z $2 && echo "Missing model file description, e.g. 'conll17_gdrive_NCI_oscar_paracrawl_wiki_filtering_basic+char-1.0+lang-0.8'"
+test -z $2 && echo "Missing BERT model type: ga_bert, multilingual_bert or electra"
 test -z $2 && exit 1
-FILE_DESC=$2
+BERT_MODEL=$2
 
-test -z $3 && echo "Missing model type, use 'generator' for MLM or 'discriminator' for classification"
+test -z $3 && echo "Missing model file description, e.g. 'conll17_gdrive_NCI_oscar_paracrawl_wiki_filtering_basic+char-1.0+lang-0.8'"
 test -z $3 && exit 1
-MODEL_TYPE=$3
+FILE_DESC=$3
+
+test -z $4 && echo "Missing model type, use 'generator' for MLM or 'discriminator' for classification"
+test -z $4 && exit 1
+MODEL_TYPE=$4
 
 # You may need to change this path according to how you set up this directory.
 GABERT_DIR="/home/${USER}/spinning-storage/${USER}/ga_BERT"
 
-CHECKPOINT="$GABERT_DIR/Irish-BERT/models/electra/${FILE_DESC}/models/electra-base-${FILE_DESC}"
-CONFIG="$GABERT_DIR/Irish-BERT/models/electra/electra_base_${MODEL_TYPE}_config.json"
-OUTDIR="$GABERT_DIR/Irish-BERT/models/electra/output/pytorch/$FILE_DESC/electra-base-${FILE_DESC}-${MODEL_TYPE}-${STEPS}"
+MODEL_DIR=models/${BERT_MODEL}
+OUTDIR=${MODEL_DIR}/output/electra_tpu_runs/${FILE_DESC}
 
-# TF checkpoints are available on Google Drive
-#CHECKPOINT="$GABERT_DIR/Irish-BERT/models/ga_bert/output/electra/$FILE_DESC/models/electra-base-irish-cased"
+PYTORCH_OUTDIR=${MODEL_DIR}/output/electra_tpu_runs/pytorch_models/electra-base-${FILE_DESC}-${MODEL_TYPE}-${STEPS}
+mkdir -p $PYTORCH_OUTDIR
 
-mkdir -p $OUTDIR
-cp "$CONFIG" "$OUTDIR/config.json"
-cp "$GABERT_DIR/Irish-BERT/models/electra/$FILE_DESC/vocab.txt" "$OUTDIR"
+CONFIG="$GABERT_DIR/Irish-BERT/models/${BERT_MODEL}/electra_base_${MODEL_TYPE}_config.json"
+cp "$CONFIG" "$PYTORCH_OUTDIR/config.json"
+
+WIKI_BERT_DIR="../wiki-bert-pipeline"
+BERT_VOCAB=${WIKI_BERT_DIR}/data/${FILE_DESC}/ga/wordpiece/cased/vocab.txt
+
+cp ${BERT_VOCAB} ${PYTORCH_OUTDIR}
 
 TRANSFORMERS_DIR="$GABERT_DIR/transformers"
 
 # Use the conversion script
 python "$TRANSFORMERS_DIR"/src/transformers/models/electra/convert_electra_original_tf_checkpoint_to_pytorch.py \
-    --tf_checkpoint_path="$CHECKPOINT/model.ckpt-$STEPS" \
+    --tf_checkpoint_path="$OUTDIR/model.ckpt-$STEPS" \
     --config_file="$CONFIG" \
-    --pytorch_dump_path="$OUTDIR"/pytorch_model.bin \
+    --pytorch_dump_path="$PYTORCH_OUTDIR"/pytorch_model.bin \
     --discriminator_or_generator="$MODEL_TYPE"
 
 # pack model as expected by AlennNLP
-cd "$OUTDIR"
+cd "$PYTORCH_OUTDIR"
 tar -czf weights.tar.gz config.json pytorch_model.bin
--- scripts/convert_multiple_electra_checkpoints.sh
+++ scripts/convert_multiple_electra_checkpoints.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-for CHKPT in 50000 100000 150000 200000 250000 300000 350000; do
+test -z $1 && echo "Missing model file description, e.g. 'conll17_gdrive_NCI_oscar_paracrawl_wiki_filtering_basic+char-1.0+lang-0.8'"
+test -z $1 && exit 1
+FILE_DESC=$1
+
+test -z $2 && echo "Missing BERT model type: ga_bert, multilingual_bert or electra"
+test -z $2 && exit 1
+BERT_MODEL=$2
+
+test -z $3 && echo "Missing model type, use 'generator' for MLM or 'discriminator' for classification"
+test -z $3 && exit 1
+MODEL_TYPE=$3
+
+OUTDIR=models/${BERT_MODEL}/output/electra_tpu_runs/${FILE_DESC}
+mkdir -p ${OUTDIR}
+
+gsutil -m cp -r gs://gabert/data/gabert-v2/pretraining_data/electra/${FILE_DESC}/models/electra-base-${FILE_DESC}/events* ${OUTDIR}
+gsutil -m cp -r gs://gabert/data/gabert-v2/pretraining_data/electra/${FILE_DESC}/models/electra-base-${FILE_DESC}/checkpoint* ${OUTDIR}
+gsutil -m cp -r gs://gabert/data/gabert-v2/pretraining_data/electra/${FILE_DESC}/models/electra-base-${FILE_DESC}/graph* ${OUTDIR}
+
+for CHKPT in 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000; do
+	gsutil -m cp -r gs://gabert/data/gabert-v2/pretraining_data/electra/${FILE_DESC}/models/electra-base-${FILE_DESC}/model.ckpt-${CHKPT}* ${OUTDIR}
+	
 	echo "converting $CHKPT checkpoint"
-	./scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh $CHKPT conll17_gdrive_NCI_oscar_paracrawl_filtering_basic+char-1.0+lang-0.8 discriminator
+	./scripts/convert_electra_original_tf_checkpoint_to_pytorch.sh ${CHKPT} ${BERT_MODEL} ${FILE_DESC} ${MODEL_TYPE}
 done
