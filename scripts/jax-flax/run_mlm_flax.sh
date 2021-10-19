1e3accc2fafbd88ad3f1f73419daf88143a36a42
jbrry <james_barry@t1v-n-c10dc02b-w-0.us-central1-a.c.gabert-v2.internal>
2021-10-19 14:03:47 +0000

    full path to train script

--- scripts/jax-flax/run_mlm_flax.sh
+++ scripts/jax-flax/run_mlm_flax.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python run_mlm_flax.py \
+python transformers/examples/flax/language-modeling/run_mlm_flax.py \
     --output_dir="./irish-roberta-base" \
     --model_type="roberta" \
     --config_name="./irish-roberta-base" \
@@ -11,7 +11,7 @@ python run_mlm_flax.py \
     --weight_decay="0.01" \
     --per_device_train_batch_size="128" \
     --per_device_eval_batch_size="128" \
-    --learning_rate="3e-4" \
+    --overwrite_output_dir \
     --warmup_steps="1000" \
     --overwrite_output_dir \
     --num_train_epochs="18" \
@@ -20,4 +20,5 @@ python run_mlm_flax.py \
     --logging_steps="500" \
     --save_steps="2500" \
     --eval_steps="2500" \
-    --push_to_hub
+    --push_to_hub 
+    
