c5912d38896b7ee31b59bffd065a682fd9994909
jbar-ry <james.barry26@mail.dcu.ie>
2021-10-26 21:55:38 +0100

    update flax scripts

--- scripts/jax-flax/run_mlm_flax.sh
+++ scripts/jax-flax/run_mlm_flax.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-python transformers/examples/flax/language-modeling/run_mlm_flax.py \
+LM_PATH="/home/jbarry/ga_BERT/transformers/examples/flax/language-modeling"
+TRAIN_PATH="/home/jbarry/ga_BERT/Irish-BERT/data/ga/oscar/train.txt"
+
+python "$LM_PATH/run_mlm_flax.py" \
     --output_dir="./irish-roberta-base" \
     --model_type="roberta" \
     --config_name="./irish-roberta-base" \
     --tokenizer_name="./irish-roberta-base" \
-    --dataset_name="oscar" \
-    --dataset_config_name="unshuffled_deduplicated_ga" \
+    --train_file="$TRAIN_PATH" \
+    --do_train --do_eval \
     --max_seq_length="128" \
     --weight_decay="0.01" \
-    --per_device_train_batch_size="128" \
-    --per_device_eval_batch_size="128" \
+    --per_device_train_batch_size="16" \
+    --per_device_eval_batch_size="16" \
     --overwrite_output_dir \
     --warmup_steps="1000" \
     --overwrite_output_dir \
@@ -18,7 +21,6 @@ python transformers/examples/flax/language-modeling/run_mlm_flax.py \
     --adam_beta1="0.9" \
     --adam_beta2="0.98" \
     --logging_steps="500" \
-    --save_steps="2500" \
-    --eval_steps="2500" \
-    --push_to_hub 
+    --save_steps="100" \
+    --eval_steps="100"
     
