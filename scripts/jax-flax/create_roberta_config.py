4862aeb4423cb367446a83c1162223827358be39
jbar-ry <james.barry26@mail.dcu.ie>
2021-10-28 23:39:49 +0100

    add grad accumulation in flax - WIP

--- scripts/jax-flax/create_roberta_config.py
+++ scripts/jax-flax/create_roberta_config.py
@@ -1,4 +1,14 @@
 from transformers import RobertaConfig
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--out",
+    default="./",
+    type=str,
+    help="Path to the output directory, where the files will be saved",
+)
+args = parser.parse_args()
 
 config = RobertaConfig.from_pretrained("roberta-base", vocab_size=30000)
-config.save_pretrained("./irish-roberta-base")
+config.save_pretrained(f"./{args.out}")
