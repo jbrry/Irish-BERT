681b54233f21c2513a8cb3fdbff1619501ad9ab3
James Barry <james.barry26@mail.dcu.ie>
2021-11-01 23:19:30 +0000

    Create train_BPE_tokenizer.py

---
+++ scripts/jax-flax/train_BPE_tokenizer.py
@@ -0,0 +1,51 @@
+import argparse
+import glob
+from os.path import join
+
+from tokenizers import ByteLevelBPETokenizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--files",
+    default=None,
+    metavar="path",
+    type=str,
+    required=True,
+    help="The files to use as training; accept '**/*.txt' type of patterns \
+                          if enclosed in quotes",
+)
+parser.add_argument(
+    "--out",
+    default="./",
+    type=str,
+    help="Path to the output directory, where the files will be saved",
+)
+args = parser.parse_args()
+
+files = glob.glob(args.files)
+if not files:
+    print(f"File does not exist: {args.files}")
+    exit(1)
+
+
+# Initialize an empty tokenizer
+tokenizer = ByteLevelBPETokenizer(add_prefix_space=True)
+
+# Special tokens
+BOS = "<s>"
+EOS = "</s>"
+UNK = "<unk>"
+PAD = "<pad>"
+MASK = "<mask>"
+
+# And then train
+tokenizer.train(
+    files,
+    vocab_size=30000,
+    min_frequency=2,
+    show_progress=True,
+    special_tokens=[BOS, PAD, EOS, UNK, MASK],
+)
+
+# Save tokenizer
+tokenizer.save(f"{args.out}/tokenizer.json")
